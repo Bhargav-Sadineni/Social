13c144664580596b4627379150de87cce32832cb
Bhargav-Sadineni <sadinenibhargav0@gmail.com>
2026-07-09 22:20:51 +0530

    inngest errors fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,14 +6,12 @@ export const inngest = new Inngest({ id: "pingup-app" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+    { id: 'sync-user-from-clerk', triggers: { event: 'clerk/user.created' } },
     async ({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
 
         let username = email_addresses[0].email_address.split('@')[0]
 
-        // Check availability of username
         const user = await User.findOne({username})
 
         if (user) {
@@ -34,14 +32,13 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest Function to update user data in a database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
+    { id: 'update-user-from-clerk', triggers: { event: 'clerk/user.updated' } },
     async ({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
 
         const updatedUserData = {
             email: email_addresses[0].email_address,
-            full_name: first_name + ' ' + last_name,
+            full_name: `${first_name || ''} ${last_name || ''}`.trim(),
             profile_picture: image_url
         }
 
@@ -51,8 +48,7 @@ const syncUserUpdation = inngest.createFunction(
 
 // Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted'},
+    { id: 'delete-user-with-clerk', triggers: { event: 'clerk/user.deleted' } },
     async ({event})=>{
         const {id} = event.data
 
@@ -60,7 +56,6 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 
-// Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
